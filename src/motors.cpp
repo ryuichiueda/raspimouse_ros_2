58a364ff4e96f7f34b228fbfa4c0807e0a4c4605
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-07-08 13:38:49 +0900

    Implement timed motion

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,9 +1,19 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <signal.h>
 #include <ros/package.h> 
 #include "raspimouse_ros_2/MotorFreqs.h"
 #include "std_srvs/Trigger.h"
+#include "raspimouse_ros_2/TimedMotion.h"
 #include <fstream>
 using namespace ros;
+
+bool setPower(bool);
+void onSigint(int);
+bool callbackOn(std_srvs::Trigger::Request&, std_srvs::Trigger::Response&);
+bool callbackOff(std_srvs::Trigger::Request&, std_srvs::Trigger::Response&);
+bool callbackTimedMotion(raspimouse_ros_2::TimedMotion::Request&, raspimouse_ros_2::TimedMotion::Response&);
+
+bool is_on = false;
 
 bool setPower(bool on)
 {
@@ -12,7 +22,14 @@ bool setPower(bool on)
 		return false;
 
 	ofs << (on ? '1' : '0') << std::endl;
+	is_on = on;
 	return true;
+}
+
+void onSigint(int sig)
+{
+	setPower(false);
+	exit(0);
 }
 
 bool callbackOn(std_srvs::Trigger::Request& request, std_srvs::Trigger::Response& response)
@@ -35,19 +52,43 @@ bool callbackOff(std_srvs::Trigger::Request& request, std_srvs::Trigger::Respons
 	return true;
 }
 
+bool callbackTimedMotion(raspimouse_ros_2::TimedMotion::Request& request, raspimouse_ros_2::TimedMotion::Response& response)
+{
+	if(not is_on){
+		ROS_INFO("Motors are not enpowered");
+		return false;
+	}
+
+	std::ofstream ofs("/dev/rtmotor0");
+	if(not ofs.is_open()){
+		ROS_ERROR("Cannot open /dev/rtmotor0");
+		return false;
+	}
+
+	ofs << request.left_hz << ' ' << request.right_hz << ' '
+		<< request.duration_ms << std::endl;
+
+	response.success = true;
+	return true;
+}
 
 int main(int argc, char **argv)
 {
 	init(argc,argv,"motors");
 	NodeHandle n;
+
+	setPower(false);
+
+	signal(SIGINT, onSigint);
+
 	ros::ServiceServer srv_on = n.advertiseService("motor_on", callbackOn);
 	ros::ServiceServer srv_off = n.advertiseService("motor_off", callbackOff);
-
-
-	ros::service::waitForService("motor_on");
-	ros::service::waitForService("motor_off");
-
-	ros::spin();
+	ros::ServiceServer srv_tm = n.advertiseService("timed_motion", callbackTimedMotion); 
+	ros::Rate loop_rate(10);
+	while(ros::ok()){
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	
 	exit(0);
 }
-
