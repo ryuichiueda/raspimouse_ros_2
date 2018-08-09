06a8f458072d86b176f4354bcf922396bffaef54
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-08-09 16:50:02 +0900

    Update to 16.04 on Travis

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -10,17 +10,17 @@ sudo pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
 bash ./step0.bash
 bash ./step1.bash
 
-sudo apt-get install ros-indigo-tf ros-indigo-tf2-geometry-msgs
+sudo apt-get install ros-kinetic-tf ros-kinetic-tf2-geometry-msgs
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
